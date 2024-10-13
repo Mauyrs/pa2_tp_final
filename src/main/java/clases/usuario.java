c2a5bea7da7221cce66759ba40294e03f827f2fc
Enooll <ignaciorosas34@gmail.com>
2024-10-13 17:19:10 -0300

    cargue clase usuario

--- src/main/java/clases/usuario.java
+++ src/main/java/clases/usuario.java
@@ -1,13 +1,96 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package clases;
 
-/**
- *
- * @author Mauri
- */
+import java.util.LinkedList;
+import java.util.List;
+
+
+
 public class usuario {
-    String nombre;
+  private String nombre;
+  private String direccion;
+  private String correo;
+  private String contrasena;
+  private int idUsuario;
+  private List<pedido>listaPedidos;
+  private carrito carritoUsu;
+  
+
+    public usuario() {
+        this.listaPedidos = new LinkedList <> () ;
+    }
+
+    public usuario(String nombre, String direccion, String correo, String contrasena, int idUsuario, List<pedido> listaPedidos, carrito carritoUsu) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.correo = correo;
+        this.contrasena = contrasena;
+        this.idUsuario = idUsuario;
+        this.listaPedidos = listaPedidos;
+        this.carritoUsu = carritoUsu;
+    }
+
+    public carrito getCarritoUsu() {
+        return carritoUsu;
+    }
+
+    public void setCarritoUsu(carrito carritoUsu) {
+        this.carritoUsu = carritoUsu;
+    }
+
+   
+
+  
+  
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public List<pedido> getListaPedidos() {
+        return listaPedidos;
+    }
+
+    public void setListaPedidos(List<pedido> listaPedidos) {
+        this.listaPedidos = listaPedidos;
+    }
+  
+    
+    
+    
 }
