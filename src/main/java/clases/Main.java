99377b1892fc831409e791f009adc57a204da486
Enooll <ignaciorosas34@gmail.com>
2024-10-24 18:21:13 -0300

    nada
    s

--- src/main/java/clases/Main.java
+++ src/main/java/clases/Main.java
@@ -3,8 +3,10 @@ package clases;
 
 import igu.Login;
 
-public class Main {
+public class main {
     public static void main(String [] args ){
+       
+
         Login log = new Login();
         log.setVisible(true);
         log.setLocationRelativeTo(null);// para que cuando se abra abre en el medio de la pantalla  
