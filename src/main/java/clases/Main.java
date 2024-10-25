b19ec0f41231a5edc802a9e4ebf607fc424162cb
Mauyrs <mauricio_ds1@outlook.com>
2024-10-25 17:08:19 -0300

    Primeras Vistas

--- src/main/java/clases/Main.java
+++ src/main/java/clases/Main.java
@@ -1,13 +1,12 @@
 
 package clases;
 
-import igu.Login;
+import igu.*;
 
 public class main {
     public static void main(String [] args ){
-       
-
         Login log = new Login();
+        
         log.setVisible(true);
         log.setLocationRelativeTo(null);// para que cuando se abra abre en el medio de la pantalla  
     }
