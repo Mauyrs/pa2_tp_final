9b41ac8e1177566a2eedf75b9659a423cba746b9
Enooll <ignaciorosas34@gmail.com>
2024-11-19 16:27:25 -0300

    Update Main.java

--- src/main/java/clases/Main.java
+++ src/main/java/clases/Main.java
@@ -6,11 +6,32 @@ import DAO.ImpDAOCategoria;
 import igu.*;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class main {
     public static void main(String [] args ){
         
         CategoriaDAO categoriaDAO = new ImpDAOCategoria();
+      //  Categoria cate = new Categoria();
+            Categoria cate = null;
+        //cate.setNombre("Juguete");
+        try {
+        /*    cate = categoriaDAO.obtener(1);
+            System.out.println(cate);
+            cate.setNombre("celulares");
+            categoriaDAO.actualizar(cate);
+            System.out.println(cate);
+          */  List <Categoria> listaCategorias = categoriaDAO.listar();
+            for(Categoria cat : listaCategorias){
+                System.out.println(cat);
+            }
+           // categoriaDAO.insertar(cate);
+        } catch (SQLException ex) {
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         
         ProductoNuevo log = new ProductoNuevo();
