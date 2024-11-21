03260d75988e53194b6e542160e74c827578ed86
Enooll <ignaciorosas34@gmail.com>
2024-11-21 01:43:43 -0300

    FIN INTERFAZ USUARIO

--- src/main/java/igu/InterfazUsuario.java
+++ src/main/java/igu/InterfazUsuario.java
@@ -35,10 +35,10 @@ public class InterfazUsuario extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         btnPerfil = new javax.swing.JButton();
         btnPronom = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setLocationByPlatform(true);
-        setUndecorated(true);
         setResizable(false);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
@@ -46,29 +46,31 @@ public class InterfazUsuario extends javax.swing.JFrame {
 
         jPanel2.setBackground(new java.awt.Color(51, 153, 255));
 
-        lblBienvenido.setFont(new java.awt.Font("Dialog", 0, 48)); // NOI18N
+        lblBienvenido.setFont(new java.awt.Font("Dialog", 0, 72)); // NOI18N
         lblBienvenido.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lblBienvenido.setText("BIENVENIDO, ENOL");
+        lblBienvenido.setText("¡BIENVENIDO!");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addComponent(lblBienvenido, javax.swing.GroupLayout.PREFERRED_SIZE, 808, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 2, Short.MAX_VALUE))
+                .addGap(135, 135, 135)
+                .addComponent(lblBienvenido, javax.swing.GroupLayout.PREFERRED_SIZE, 531, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(194, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(lblBienvenido, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(17, Short.MAX_VALUE)
+                .addComponent(lblBienvenido, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
-        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 810, 120));
+        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 860, 110));
 
         btnProcat.setText("BUSCAR PRODUCTO POR CATEGORIA");
-        jPanel1.add(btnProcat, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 310, 240, 60));
+        jPanel1.add(btnProcat, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 340, 250, 60));
 
         btnPedidos.setText("MIS PEDIDOS");
         btnPedidos.addActionListener(new java.awt.event.ActionListener() {
@@ -76,21 +78,26 @@ public class InterfazUsuario extends javax.swing.JFrame {
                 btnPedidosActionPerformed(evt);
             }
         });
-        jPanel1.add(btnPedidos, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 240, 240, 60));
+        jPanel1.add(btnPedidos, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 260, 250, 60));
 
         btnCarrito.setText("VER MI CARRITO");
-        jPanel1.add(btnCarrito, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 240, 240, 60));
+        jPanel1.add(btnCarrito, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 260, 250, 60));
 
         jLabel1.setFont(new java.awt.Font("Dialog", 0, 30)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("¿QUE DESEAS HACER?");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 150, 390, 40));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 200, 390, 40));
 
         btnPerfil.setText("VER MI PERFIL");
-        jPanel1.add(btnPerfil, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 240, 240, 60));
+        jPanel1.add(btnPerfil, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 260, 250, 60));
 
         btnPronom.setText("BUSCAR PRODUCTO POR NOMBRE");
-        jPanel1.add(btnPronom, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 310, 240, 60));
+        jPanel1.add(btnPronom, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 340, 250, 60));
+
+        jLabel2.setFont(new java.awt.Font("Dialog", 0, 68)); // NOI18N
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("ENOL");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 110, 220, 70));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -100,7 +107,7 @@ public class InterfazUsuario extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 479, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 460, Short.MAX_VALUE)
         );
 
         pack();
@@ -119,6 +126,7 @@ public class InterfazUsuario extends javax.swing.JFrame {
     private javax.swing.JButton btnProcat;
     private javax.swing.JButton btnPronom;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JLabel lblBienvenido;
