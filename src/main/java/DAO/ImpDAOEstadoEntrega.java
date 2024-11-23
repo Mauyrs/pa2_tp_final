8ddcb9145e280eb77e081c7d358b2492368116a8
Mauyrs <mauricio_ds1@outlook.com>
2024-11-23 12:03:11 -0300

    Item e interfaz pedido
    falta la funcionalidad

--- src/main/java/DAO/ImpDAOEstadoEntrega.java
+++ src/main/java/DAO/ImpDAOEstadoEntrega.java
@@ -4,8 +4,14 @@
  */
 package DAO;
 
+import clases.Categoria;
 import clases.EstadoEntrega;
+import conexiones.ConSql;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,12 +22,47 @@ public class ImpDAOEstadoEntrega implements EstadoEntregaDAO {
 
     @Override
     public EstadoEntrega obtener(int id) throws SQLException, ClassNotFoundException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Connection con = ConSql.obtener();
+        EstadoEntrega est = null;
+        String sql = "SELECT descripcion FROM categoria WHERE id = ?";
+        PreparedStatement prep = con.prepareStatement(sql);
+
+        prep.setInt(1, id);
+
+        ResultSet rs = prep.executeQuery();
+
+        if(rs.next()){
+            est = new EstadoEntrega(id,rs.getString("descripcion"));
+        }
+
+        ConSql.cerrarConexion(con);
+        ConSql.cerrarPrepStmt(prep);
+        ConSql.cerrarResultSet(rs);
+        return est;
+        
     }
 
     @Override
     public List<EstadoEntrega> listar() throws SQLException, ClassNotFoundException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Connection con = ConSql.obtener();
+        List<EstadoEntrega> estados = new ArrayList();
+        String sql = "SELECT id, descripcion FROM categoria ";
+        PreparedStatement prep = con.prepareStatement(sql);
+
+        ResultSet rs = prep.executeQuery();
+
+        while(rs.next()){
+            Integer id = rs.getInt("id");
+            EstadoEntrega est = new EstadoEntrega(id,rs.getString("descripcion"));
+            estados.add(est);
+            
+        }
+
+        ConSql.cerrarConexion(con);
+        ConSql.cerrarPrepStmt(prep);
+        ConSql.cerrarResultSet(rs);
+        return estados;
+        
     }
 
     @Override
